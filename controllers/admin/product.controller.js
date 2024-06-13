dd4fe6f1be482776d53224d6dab88fe89c64e8b9
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-06-13 22:47:55 +0700

    3.1. Thay đổi trạng thái một sản phẩm - Gửi lên bằng phương thức GET

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const paginationHelper = require("../../helpers/pagination.helper");
 
 module.exports.index = async (req, res) => {
     const find = {
@@ -31,18 +32,7 @@ module.exports.index = async (req, res) => {
     // Hết tìm kiếm
 
     // Phân trang
-    const pagination = {
-        currentPage : 1,
-        limitItems : 4
-    };
-    if(req.query.page){
-        pagination.currentPage = parseInt(req.query.page);
-    }
-    pagination.skip = (pagination.currentPage-1)*pagination.limitItems;
-    
-    const countProducts = await Product.countDocuments(find);
-    const totalPage = Math.ceil(countProducts/pagination.limitItems);
-    pagination.totalPage = totalPage;
+    const pagination =  await paginationHelper(req,find);
 
     // Hết phân trang
 
