ea20be22340a352f7df2b4709be91baa9883d9cd
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-06-15 21:08:30 +0700

    3.2. Thay đổi trạng thái nhiều sản phẩm - Backend nhận data và lưu vào database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 require('dotenv').config()
+const bodyParser = require('body-parser');
 
 const database = require("./config/database");
 database.connect();
@@ -17,6 +18,8 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static('public'));
+// parse application/json
+app.use(bodyParser.json());
 
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;// chỉ dùng trong các file pug
