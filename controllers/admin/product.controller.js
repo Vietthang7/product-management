15d02620808b6429d71e64f70021fb2dea59c9dc
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-06-15 19:50:45 +0700

    3.2. Thay đổi trạng thái nhiều sản phẩm - Logic Frontend gửi data cho backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,69 +3,76 @@ const paginationHelper = require("../../helpers/pagination.helper");
 
 module.exports.index = async (req, res) => {
     const find = {
-        deleted : false
+        deleted: false
     }
-    const filterStatus = [
-        {
-            label : "Tất cả",
-            value : ""
+    const filterStatus = [{
+            label: "Tất cả",
+            value: ""
         },
         {
-            label : "Đang hoạt động",
-            value : "active"
+            label: "Đang hoạt động",
+            value: "active"
         },
         {
-            label : "Dừng hoạt động",
-            value : "inactive"
+            label: "Dừng hoạt động",
+            value: "inactive"
         },
     ];
-    if(req.query.status){
+    if (req.query.status) {
         find.status = req.query.status;
     }
     // Tìm kiếm 
     let keyword = "";
-    if(req.query.keyword){
-        const regex = new RegExp(req.query.keyword,"i");
-        find.title =  regex;
+    if (req.query.keyword) {
+        const regex = new RegExp(req.query.keyword, "i");
+        find.title = regex;
         keyword = req.query.keyword;
     }
     // Hết tìm kiếm
 
     // Phân trang
-    const pagination =  await paginationHelper(req,find);
+    const pagination = await paginationHelper(req, find);
 
     // Hết phân trang
 
     const products = await Product
         .find(find)
-        .limit(pagination.limitItems)// số lượng tối thiểu 
-        .skip(pagination.skip);// bỏ qua 
+        .limit(pagination.limitItems) // số lượng tối thiểu 
+        .skip(pagination.skip); // bỏ qua 
 
 
-    
+
     // console.log(products);
-    res.render("admin/pages/products/index",{
-        pageTitle : "Trang danh sách sản phẩm ",
-        products : products,
-        keyword : keyword,
-        filterStatus : filterStatus,
-        pagination:pagination
-    }
-    );
+    res.render("admin/pages/products/index", {
+        pageTitle: "Trang danh sách sản phẩm ",
+        products: products,
+        keyword: keyword,
+        filterStatus: filterStatus,
+        pagination: pagination
+    });
 }
 // [PATCH] /admin/products/change-status/:statusChange/:id
-module.exports.changeStatus = async(req,res) =>{
-    const {id,statusChange} = req.params;
+module.exports.changeStatus = async (req, res) => {
+    const {
+        id,
+        statusChange
+    } = req.params;
     await Product.updateOne({
-        _id :id
+        _id: id
 
-    },{
-       status : statusChange
+    }, {
+        status: statusChange
     });
     res.json({
-      code : 200
+        code: 200
     });
     // res.json() trả về api
     // res.redirect trả về route
     // res.render trả về file pug
 }
+// [PATCH] /admin/products/change-multi
+module.exports.changeMulti = async (req, res) => {
+    res.json({
+      code: 200
+    });
+  }
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,4 +5,6 @@ const router = express.Router();
 const controller = require("../../controllers/admin/product.controller");
 router.get("/",controller.index);
 router.patch("/change-status/:statusChange/:id",controller.changeStatus);
+router.patch("/change-multi",controller.changeMulti);
+
 module.exports = router;
