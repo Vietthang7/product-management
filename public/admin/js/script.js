f882bf913e38c806a12bcacd996ccdc2f6b828cb
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-06-22 09:17:25 +0700

    3.2. Thay đổi trạng thái nhiều sản phẩm - Tối ưu code

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -135,7 +135,9 @@ if (boxActions) {
                 status: status,
                 ids: ids
             };
-            fetch("/admin/products/change-multi", {
+            const link = boxActions.getAttribute("box-actions");
+
+            fetch(link, {
                     method: "PATCH",
                     headers: {
                         "Content-Type": "application/json",
