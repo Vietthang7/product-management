70e757c87f088111917e19bd9954c2bd9b3ef670
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-06-24 22:09:29 +0700

    3.1. Hiển thị thông báo sau khi đổi trạng thái

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 require('dotenv').config()
 const bodyParser = require('body-parser');
+// const flash = require('express-flash');
+// const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const database = require("./config/database");
 database.connect();
@@ -13,7 +19,15 @@ const systemConfig = require("./config/system");
 const app = express();
 const port = process.env.PORT;
 
+//Flash
+// app.use(cookieParser('HHKALKS'));
+// app.use(session({cookie:{maxAge:60000}}));
+// app.use(flash());
+app.use(cookieParser('HHKALKS'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
 
+//End Flash
 app.set("views", "./views");
 app.set("view engine", "pug");
 
