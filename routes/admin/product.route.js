cba79d15f08b765faf2e218e2f97d386d042d21b
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-06-27 17:44:33 +0700

    4. Tính năng tạo mới sản phẩm - Upload ảnh

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer')
+const storageMulterHelper = require("../../helpers/storageMulter.helper");
+
 
 
 const controller = require("../../controllers/admin/product.controller");
+const upload = multer({ storage: storageMulterHelper.storage });
 router.get("/", controller.index);
 router.patch("/change-status/:statusChange/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 
 router.patch("/delete/:id", controller.deleteItem);
-router.patch("/change-position/:id",controller.changePosition);
-router.get("/create",controller.create);
-router.post("/create",controller.createPost);
+router.patch("/change-position/:id", controller.changePosition);
+router.get("/create", controller.create);
+router.post("/create",
+    upload.single('thumbnail'),
+    controller.createPost);
 
 
 module.exports = router;
