2d4b3bd2eae08e660e84c1a2a0d56554ba794eda
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-02 10:10:59 +0700

    4. Làm trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,21 +1,82 @@
-// [GET] /products/
+// // [GET] /products/
+// const Product = require("../../models/product.model");
+// module.exports.index = async (req, res) => {
+//     const products = await Product
+//         .find({
+//             status: "active",
+//             deleted: true
+//         })
+//         .sort({
+//             position: "desc"
+//         });
+//     for (const item of products) {
+//         item.priceNew = ((1 - item.discountPercentage / 100) * item.price).toFixed(0);
+//     }
+//     // console.log(products);
+//     res.render("client/pages/products/index", {
+//         pageTitle: "Danh sách sản phẩm",
+//         products: products
+//     }
+//     );
+// }
+// //[GET]/products/detail
+// module.exports.detail = async (req, res) => {
+//     const slug = req.params.slug;
+//     const product = await Product.findOne({
+//         slug: slug,
+//         deleted: false,
+//         status: "active"
+//     });
+//     console.log(product);
+//     if (product) {
+//         res.render("client/pages/products/detail", {
+//             pageTitle: "Chi tiết sản phẩm",
+//             product: product
+//         });
+//     } else {
+//         res.redirect("/");
+//     }
+
+// }
 const Product = require("../../models/product.model");
+// [GET] /products/
 module.exports.index = async (req, res) => {
-    const products = await Product 
+  const products = await Product
     .find({
-        status : "active",
-        deleted : true
+      status: "active",
+      deleted: false
     })
     .sort({
-        position : "desc"
+      position: "desc"
     });
-    for (const item of products) {
-        item.priceNew = ((1 - item.discountPercentage / 100) * item.price).toFixed(0);
-    }
-    // console.log(products);
-    res.render("client/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm",
-        products: products
-    }
-    );
+  for (const item of products) {
+    item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+  }
+  // console.log(products);
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products
+  });
+}
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+  const slug = req.params.slug;
+
+  const product = await Product.findOne({
+    slug: slug,
+    deleted: false,
+    status: "active"
+  });
+
+  console.log(product);
+
+  if(product) {
+    res.render("client/pages/products/detail", {
+      pageTitle: "Chi tiết sản phẩm",
+      product: product
+    });
+  } else {
+    res.redirect("/");
+  }
 }
