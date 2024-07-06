fdba68bfd864e3de7be31f6003e1b2540b860eba
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-06 13:13:49 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Thêm mới danh mục)

--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer')
+const uploadToCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const upload = multer();
+const validate = require("../../validates/admin/product.validate");
+
 
 
 const controller = require("../../controllers/admin/product-category.controller");
 router.get("/",controller.index);
+router.get("/create",controller.index);
+router.post("/create",
+  upload.single('thumbnail'),
+  uploadToCloud.uploadSingle,
+  validate.createPost,
+  controller.createPost
+);
 
 module.exports = router;
