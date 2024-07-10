9e766f16fddd0db9e57b432e52a39225a54c2a85
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-10 17:15:41 +0700

    2.2. Phần chỉnh sửa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -177,9 +177,14 @@ module.exports.edit = async (req, res) => {
       deleted: false
     });
     if (product) {
+      const categories = await ProductCategory.find({
+        deleted: false
+      });
+      const newCategories = createTreeHelper(categories);
       res.render("admin/pages/products/edit", {
         pageTitle: "Chỉnh sửa sản phẩm",
-        product: product
+        product: product,
+        categories:newCategories
       });
     } else {
       res.redirect(`/${systemConfig.prefixAdmin}/products`);
