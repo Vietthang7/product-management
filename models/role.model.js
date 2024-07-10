7db06c91ad9923da70df5176a8199f9e8cf97818
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-10 21:22:01 +0700

    3.1. Tạo collection roles

---
+++ models/role.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const roleSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    permissions :{
+      type:Array,
+      default : []
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+},{
+    timestamps: true // Tự động thêm trường createdAt và updatedAt (https://mongoosejs.com/docs/timestamps.html)
+});
+const Role = mongoose.model('Role', roleSchema, "roles");
+
+module.exports = Role;
