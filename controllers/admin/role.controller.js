e7510594ae2916ae3e1b00cf8fe15ccd2cb9ba0c
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-10 22:12:04 +0700

    3.4. Xây dựng phần Chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -23,3 +23,37 @@ module.exports.createPost = async (req, res) => {
   await records.save();
   res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 };
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const records = await Role.findOne({
+      _id: id,
+      deleted: false
+    });
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Chỉnh sửa nhóm quyền",
+      records:records
+    }
+    );
+
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Role.updateOne({
+      _id: id,
+      deleted: false
+    }, req.body);
+    req.flash("success", "Cập nhật sản phẩm thành công!");
+    res.redirect("back");
+
+  } catch (error) {
+    req.flash("error", "Id sản phẩm không hợp lệ !");
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+  }
+};
