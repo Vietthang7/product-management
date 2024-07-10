db3a70643856c8cfb3b1514dfb962d59ed06a199
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-10 21:38:47 +0700

    3.2. Xây dựng phần Danh sách nhóm quyền

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,13 +2,15 @@ const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const systemConfig = require("../../config/system");
 const productCategoryRoute = require("./product-category.route");
+const rolesRoute = require("./role.route");
 
 module.exports.index = (app) => {
-    const path =`/${systemConfig.prefixAdmin}`;
+    const path = `/${systemConfig.prefixAdmin}`;
 
     app.use(`${path}/dashboard`, dashboardRoute);
-    
+
     app.use(`${path}/products`, productRoute);
     app.use(`${path}/products-category`, productCategoryRoute);
+    app.use(`${path}/roles`, rolesRoute);
 }
 
