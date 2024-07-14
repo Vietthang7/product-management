1016e96ba3de1e48d6ed6d3cfe55dba7ca763b13
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-14 10:22:52 +0700

    4. Thêm middleware cho route private

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,16 +5,28 @@ const productCategoryRoute = require("./product-category.route");
 const rolesRoute = require("./role.route");
 const accountsRoute = require("./account.route");
 const authRoute = require("./auth.route");
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
+
 
 module.exports.index = (app) => {
     const path = `/${systemConfig.prefixAdmin}`;
 
-    app.use(`${path}/dashboard`, dashboardRoute);
+    app.use(`${path}/dashboard`,
+        authMiddleware.requireAuth,
+        dashboardRoute);
 
-    app.use(`${path}/products`, productRoute);
-    app.use(`${path}/products-category`, productCategoryRoute);
-    app.use(`${path}/roles`, rolesRoute);
-    app.use(`${path}/accounts`, accountsRoute);
+    app.use(`${path}/products`,
+        authMiddleware.requireAuth,
+        productRoute);
+    app.use(`${path}/products-category`,
+        authMiddleware.requireAuth,
+        productCategoryRoute);
+    app.use(`${path}/roles`,
+        authMiddleware.requireAuth,
+        rolesRoute);
+    app.use(`${path}/accounts`,
+        authMiddleware.requireAuth, 
+        accountsRoute);
     app.use(`${path}/auth`, authRoute);
 }
 
