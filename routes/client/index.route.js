cac6c1d102d7e7e73b7c7ab18abaf39a6818884c
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-23 16:48:20 +0700

    6. Trang chi tiết sản phẩm bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,10 @@
 const homeRoute = require("./home.route");
 const productRoute = require("./product.route");
 const categoryMiddleware = require("../../middlewares/client/category.middlewares");
-
+const searchRoute = require("./search.route");
 module.exports.index = (app) => {
     app.use(categoryMiddleware.category);
     app.use("/", homeRoute);
-
+    app.use("/search",searchRoute);
     app.use("/products", productRoute);
 }
