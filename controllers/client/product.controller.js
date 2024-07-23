bd5d7fd94c2e1c31d4f2801bcf74a7f6103fb331
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-23 15:31:50 +0700

    6. Trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -27,34 +27,34 @@ module.exports.category = async (req, res) => {
     deleted: false
   });
   const allSubCategory = [];
-  const getSubCategory = async (currentId) =>{
+  const getSubCategory = async (currentId) => {
     const SubCategory = await ProductCategory.find({
-      parent_id : currentId,
-      status:"active",
-      deleted : false
+      parent_id: currentId,
+      status: "active",
+      deleted: false
     });
-    for(const sub of SubCategory){
+    for (const sub of SubCategory) {
       allSubCategory.push(sub.id);
       await getSubCategory(sub.id);
     }
   }
   await getSubCategory(category.id);
   const products = await Product
-  .find({
-    product_category_id :{
-      $in:[
-        category.id,
-        ...allSubCategory
-      ]
-    },
-    status:"active",
-    deleted:false
-  })
-  .sort({
-    position:"desc"
-  });
+    .find({
+      product_category_id: {
+        $in: [
+          category.id,
+          ...allSubCategory
+        ]
+      },
+      status: "active",
+      deleted: false
+    })
+    .sort({
+      position: "desc"
+    });
   for (const item of products) {
-    item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    item.priceNew = ((1 - item.discountPercentage / 100) * item.price).toFixed(0);
   }
   res.render("client/pages/products/index", {
     pageTitle: category.title,
@@ -71,9 +71,7 @@ module.exports.detail = async (req, res) => {
     deleted: false,
     status: "active"
   });
-
-  //   console.log(product);
-
+  product.priceNew = ((1 - product.discountPercentage / 100) * product.price).toFixed(0);
   if (product) {
     res.render("client/pages/products/detail", {
       pageTitle: "Chi tiết sản phẩm",
