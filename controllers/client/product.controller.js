b4aa5782a5dd51c9e7ad6369eeabcaff6effe883
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-23 11:02:38 +0700

    Làm trang chủ bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,43 +1,3 @@
-// // [GET] /products/
-// const Product = require("../../models/product.model");
-// module.exports.index = async (req, res) => {
-//     const products = await Product
-//         .find({
-//             status: "active",
-//             deleted: true
-//         })
-//         .sort({
-//             position: "desc"
-//         });
-//     for (const item of products) {
-//         item.priceNew = ((1 - item.discountPercentage / 100) * item.price).toFixed(0);
-//     }
-//     // console.log(products);
-//     res.render("client/pages/products/index", {
-//         pageTitle: "Danh sách sản phẩm",
-//         products: products
-//     }
-//     );
-// }
-// //[GET]/products/detail
-// module.exports.detail = async (req, res) => {
-//     const slug = req.params.slug;
-//     const product = await Product.findOne({
-//         slug: slug,
-//         deleted: false,
-//         status: "active"
-//     });
-//     console.log(product);
-//     if (product) {
-//         res.render("client/pages/products/detail", {
-//             pageTitle: "Chi tiết sản phẩm",
-//             product: product
-//         });
-//     } else {
-//         res.redirect("/");
-//     }
-
-// }
 const Product = require("../../models/product.model");
 // [GET] /products/
 module.exports.index = async (req, res) => {
@@ -52,7 +12,6 @@ module.exports.index = async (req, res) => {
   for (const item of products) {
     item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
   }
-  // console.log(products);
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
     products: products
