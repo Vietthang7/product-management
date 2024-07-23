a911e66d194af3bfaed2af264cca5a28f8f5b863
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-23 20:28:42 +0700

    2.2. Làm nút Mini Cart trên Header

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,11 +2,9 @@ const homeRoute = require("./home.route");
 const productRoute = require("./product.route");
 const categoryMiddleware = require("../../middlewares/client/category.middlewares");
 const searchRoute = require("./search.route");
-<<<<<<< HEAD
 const cartRoute = require("./cart.route");
 const cartMiddleware = require("../../middlewares/client/cart.middleware");
-=======
->>>>>>> cac6c1d102d7e7e73b7c7ab18abaf39a6818884c
+
 module.exports.index = (app) => {
     app.use(categoryMiddleware.category);
     app.use(cartMiddleware.cartId);
