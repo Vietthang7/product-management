81936ae386df47d9a87855632c5d96ef99650e86
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-25 16:50:12 +0700

    1.4. Làm trang đặt hàng thành công

--- routes/client/checkout.route.js
+++ routes/client/checkout.route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const validate = require("../../validates/client/order.validate.js");
 
 const controller = require("../../controllers/client/checkout.controller.js");
-router.get("/",controller.index);
+router.get("/", controller.index);
 router.post("/order",
-  validate.inFoUserOrder, 
-  controller.orderPost);
-
+  validate.inFoUserOrder,
+  controller.orderPost
+);
+router.get("/success/:orderId", controller.success);
 module.exports = router;
