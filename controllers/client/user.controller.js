e401570a7a5bd4ea934355e4c104008069572297
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-31 09:55:31 +0700

    3. Quên mật khẩu - Hướng dẫn gửi OTP qua email tự động

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -2,6 +2,7 @@ const md5 = require("md5");
 const generateHelper = require("../../helpers/generate.helper");
 const User = require("../../models/user.model");
 const ForgotPassword = require("../../models/forgot-password.model");
+const sendMailHelper = require("../../helpers/sendEmail.helper");
 //[GET] /user/register
 module.exports.register = async (req, res) => {
   res.render("client/pages/user/register", {
@@ -110,7 +111,10 @@ module.exports.forgotPasswordPost = async (req, res) => {
   await forgotPassword.save();
 
 
-  // Việc 2: Gửi mã OTP qua email của user (Tạm thời coi như xong, làm sau)
+  // Việc 2: Gửi mã OTP qua email của user 
+  const subject = "Mã OTP lấy lại mật khẩu."
+  const htmlSendMail =`Mã OTP xác thực của bạn là <b style = "color:green;"> ${otp} </b>.Mã OTP có hiệu lực trong 3 phút .Vui lòng không cung cấp mã OTP cho người khác`;
+  sendMailHelper.sendMail(email,subject,htmlSendMail);
   res.redirect(`/user/password/otp?email=${email}`);
 };
 
