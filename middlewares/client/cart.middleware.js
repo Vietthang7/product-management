38f538061c1c281279c699fbef8207b10bbb41b0
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-07-31 10:14:33 +0700

    update

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,6 +1,6 @@
 const Cart = require("../../models/cart.model");
 module.exports.cartId = async (req, res, next) => {
-  if(!req.cookies.cartId) {
+  if (!req.cookies.cartId) {
     const cart = new Cart();
     await cart.save();
     const expires = 365 * 24 * 60 * 60 * 1000;
@@ -8,14 +8,24 @@ module.exports.cartId = async (req, res, next) => {
       "cartId",
       cart.id,
       {
-        expires : new Date(Date.now() + expires)
-      }
-    );
+        expires: new Date(Date.now() + expires)
+      });
   } else {
     const cart = await Cart.findOne({
-      _id : req.cookies.cartId
+      _id: req.cookies.cartId
     });
-    res.locals.cartTotal = cart.products.length || 0;
+    if (cart) {
+      res.locals.cartTotal = cart.products.length || 0;
+    } else {
+      // Nếu không tìm thấy cart, bạn có thể quyết định tạo mới hoặc xử lý theo cách khác  
+      const newCart = new Cart();
+      await newCart.save();
+      const expires = 365 * 24 * 60 * 60 * 1000;
+      res.cookie("cartId", newCart.id, {
+        expires: new Date(Date.now() + expires)
+      });
+      res.locals.cartTotal = 0; // Hoặc bất kỳ giá trị nào bạn muốn gán  
+    }
   }
   next();
 }
