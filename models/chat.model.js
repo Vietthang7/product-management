59bd69578012f7d289bba1db4b7393eb1d57d3e9
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-08-05 13:56:08 +0700

    5.1. Tạo collection chats

---
+++ models/chat.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  userId : String,
+  // roomChatId : String,
+  content : String,
+  images : Array
+},{
+    timestamps: true // Tự động thêm trường createdAt và updatedAt (https://mongoosejs.com/docs/timestamps.html)
+});
+const Chat = mongoose.model("Chat", chatSchema, "chats");
+
+module.exports = Chat;
