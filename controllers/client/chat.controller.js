e1aade7343ab3a3b63cdc728ee500a8168fbe9a1
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-08-05 15:28:34 +0700

    5.2. Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,10 +1,31 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 //[GET] /chat 
 module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
   //SocketIO
-  _io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
+  _io.once('connection', (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      const chatData = {
+        userId : userId,
+        content : data.content
+      };
+      // Lưu vào database 
+      const chat = new Chat(chatData);
+      await chat.save();
+    })
   });
+  //End SocketIO
+  const chats = await Chat.find({});
+  console.log(chats);
+  for(const chat of chats){
+    const infoUser = await User.findOne({
+      _id : chat.userId
+    });
+    chat.fullName = infoUser.fullName;
+  }
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats : chats
   });
 };
