b6c0146f947bd32ccc79a48e389040d2aafd95c1
vietthang7 <nguyenvietthang12092004@gmail.com>
2024-08-23 09:57:36 +0700

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-require('dotenv').config()
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
